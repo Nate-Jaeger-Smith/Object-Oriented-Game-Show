5119ff5aee141e6e8cc92535585ce8ac8fedc884
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-13 14:14:36 -0600

    Added logic for addPhraseToDisplay

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,8 +2,28 @@ class Phrase {
     constructor(phrase){
         this.phrase = phrase.toLowerCase()
     }
+    
+    
     addPhraseToDisplay(){
         const phraseDivUl = document.querySelector('#phrase ul');
-        
+        const phraseArray = [...this.phrase];
+
+        phraseArray.forEach( letter => {
+            if (letter !== ' ') {
+                let letterLi = `<li class="hide letter ${letter}">${letter}</li>`;
+                phraseDivUl.insertAdjacentElement('beforeend', letterLi);
+            } else {
+                let spaceLi = `<li class="space"> </li>`;
+                phraseDivUl.insertAdjacentElement('beforeend', spaceLi);
+            }
+        });
+    }
+
+    checkLetter(){
+
+    }
+
+    showMatchedLetter(){
+
     }
 };
