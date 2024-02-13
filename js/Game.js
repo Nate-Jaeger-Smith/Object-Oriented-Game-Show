6b2595bef2213e82e95ae37aef1ebfd9d0fa6e91
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-13 13:32:27 -0600

    Added phrases array to Game class

--- js/Game.js
+++ js/Game.js
@@ -4,7 +4,23 @@
 class Game {
     constructor(phrase){
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = [
+            {
+                phrase: 'A red herring'
+            },
+            {
+                phrase: 'Cool as ice'
+            },
+            {
+                phrase: 'Ahead of the curve'
+            },
+            {
+                phrase: 'It suddenly dawned on me'
+            },
+            {
+                phrase: 'Break Your old routines'
+            }
+        ];
         this.activePhrase = null;
     }
 }
