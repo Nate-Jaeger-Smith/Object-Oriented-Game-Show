667c40828e4515a3830d2e7ccbbbdfe9f186b7e5
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-14 00:31:55 -0600

    Added startGame and event listener on overlay button

--- js/Game.js
+++ js/Game.js
@@ -4,29 +4,24 @@
 class Game {
     constructor(){
         this.missed = 0;
-        this.phrases = [
-            {
-                phrase: 'A red herring'
-            },
-            {
-                phrase: 'Cool as ice'
-            },
-            {
-                phrase: 'Ahead of the curve'
-            },
-            {
-                phrase: 'It suddenly dawned on me'
-            },
-            {
-                phrase: 'Break Your old routines'
-            }
+        this.phrases = [ new Phrase('A red herring'),
+            new Phrase('Cool as ice'),
+            new Phrase('Ahead of the curve'),
+            new Phrase('It suddenly dawned on me'),
+            new Phrase('Break your old routines')
         ];
         this.activePhrase = null;
     }
 
     getRandomPhrase(){
         const randomNumber = Math.floor( Math.random() * this.phrases.length );
-        return this.phrases[randomNumber].phrase;
+        return this.phrases[randomNumber];
     }
     
+    startGame(){
+        const overlay = document.getElementById('overlay');
+        overlay.style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
 }
--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@
  * Project 4 - OOP Game App
  * app.js */
 
+const startButton = document.getElementById('btn__reset');
+
+
+startButton.addEventListener('click', e => {
+    const game = new Game();
+    game.startGame();
+    console.log(game.activePhrase);
+});
