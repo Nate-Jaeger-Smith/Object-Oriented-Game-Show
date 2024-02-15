cdcbd0a82e018648a305bf2511d1da2edbd8fe7a
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-15 14:00:48 -0600

    Added eventListener for clicks on keyboard
    Also moved game variable to global scope so I can select and run testing on it in JS console

--- js/app.js
+++ js/app.js
@@ -2,10 +2,19 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const startButton = document.getElementById('btn__reset');
+const startButton = document.getElementById('btn__reset'),
+    keyboard = document.getElementById('qwerty');
+let game;
 
 
 startButton.addEventListener('click', e => {
-    const game = new Game();
+    game = new Game();
     game.startGame();
+});
+
+keyboard.addEventListener('click', e => {
+
+    if (e.target.tagName === 'BUTTON') {
+        const letter = e.target.textContent;
+    }
 });
