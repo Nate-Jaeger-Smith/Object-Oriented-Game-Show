1a343c5d44d65f9ad822c2709b2983aec51908d2
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-02-16 01:38:41 -0600

    Created resetGame()
    Game will now reset. Keyboard gets cleared, previous phrase is cleared, health is set back to full

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let game;
 
 
 startButton.addEventListener('click', e => {
+    resetGame();
     game = new Game();
     game.startGame();
 });
@@ -13,3 +14,28 @@ keyboard.addEventListener('click', e => {
         game.handleInteraction(e.target);
     }
 });
+
+/**
+ * Reset game for next round
+ * Clear on-screen keyboard
+ * Clear previous phrase display
+ * Set health back to full
+ */
+function resetGame(){
+    const keys = keyboard.getElementsByClassName('key');
+    const phraseUL = document.querySelector('#phrase ul');
+    const lives = document.getElementsByTagName('img');
+
+    [...keys].forEach( key => {
+        key.disabled = false;
+        key.setAttribute('class', 'key');
+    });
+
+    while (phraseUL.firstChild) {
+        phraseUL.removeChild(phraseUL.firstChild);
+    }
+
+    [...lives].forEach( heart => {
+        heart.setAttribute('src', 'images/liveHeart.png');
+    });
+}
